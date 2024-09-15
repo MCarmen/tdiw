5c5a0aac7f26854b9f92c45b8b02029659051aa5
Miguel Carpio <mcarpio@redhat.com>
2024-09-15 17:05:28 +0200

    Update script comments

--- p4.sh
+++ p4.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+#Create Dockerfile
 cat <<EOF > Dockerfile
 FROM php:7.2-apache
 
@@ -12,20 +13,26 @@ RUN apt-get install -y libpq-dev \
     && docker-php-ext-install pdo pdo_pgsql pgsql
 EOF
 
+#Docker build
 docker build -t apache-php-pdo_pgsql .
 
+#Docker with apache-php-pdo_pgsql
 docker run -d --name p4-php --network="host" -v "$PWD":/var/www/html apache-php-pdo_pgsql:latest
 
+#Create PostgreSQL DB Docker
 docker run --name p4-db -p 5432:5432 -e POSTGRES_USER=david -e POSTGRES_DB=myDB -e POSTGRES_PASSWORD=password -d postgres
 
+#Wait ten seconds for DB to be ready
 sleep 10
 
 export PGPASSWORD=password
 
+#Create graus table
 psql -v ON_ERROR_STOP=1 -U david -h localhost -w myDB <<-EOSQL
 	CREATE TABLE graus (id varchar(1), nom VARCHAR (50) UNIQUE NOT NULL); 
 EOSQL
 
+#Create mencions table
 psql -v ON_ERROR_STOP=1 -U david -h localhost -w myDB <<-EOSQL
         CREATE TABLE mencions (id SERIAL varchar(1), nom VARCHAR (50) UNIQUE NOT NULL, grau INT);
 EOSQL
@@ -38,6 +45,7 @@ psql -v ON_ERROR_STOP=1 -U david -h localhost -w myDB <<-EOSQL
 	INSERT INTO graus (id, nom) VALUES ('Q', 'Enginyeria Química');
 EOSQL
 
+#Insert mencions
 psql -v ON_ERROR_STOP=1 -U david -h localhost -w myDB <<-EOSQL
         INSERT INTO mencions (id, nom, grau) VALUES ('S', 'Enginyeria del software', 1);
         INSERT INTO mencions (id, nom, grau) VALUES ('A', 'Enginyeria de computadors', 1);
